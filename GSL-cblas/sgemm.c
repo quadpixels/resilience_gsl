0e39625f3bb9b77883366610a1b27e47ff294cc7
Sui Chen <cs900601@qb4.(none)>
2012-10-09 14:21:53 -0500

    Before launching a script to test branches 20120817 and urb

--- GSL-cblas/sgemm.c
+++ GSL-cblas/sgemm.c
@@ -12,18 +12,8 @@ static const int fault_limit_mm = 10;
 static jmp_buf buf_mm;
 static INDEX i, j, k;
 
-static INDEX i, j, k;
-static volatile INDEX i_1, j_1, k_1;
-static volatile INDEX *p_i, *p_j, *p_k;
-
 static void gemm_handler(int sig, siginfo_t* si, void* unused) {
-	printf("[sgemm_handler]\n");
-	printf(" >> Caught SIGSEGV signal (%d out of %d allowed)",
-		fault_count_mm, fault_limit_mm);
-	printf(" >> i=%d, j=%d, k=%d\n",
-		*p_i, *p_j, *p_k);
-	i_1 = *p_i; j_1 = *p_j; k_1 = *p_k;
-
+	puts(" >> Routine rollback (MM)");
 	if(fault_count_mm < fault_limit_mm)
 		siglongjmp(buf_mm, 1);
 	else
