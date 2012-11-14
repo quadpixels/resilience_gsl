b18719b7143cfe0d9cb001f5daa705a162a1165b
Tommy Chen <suichen6@gmail.com>
2012-11-14 16:49:50 -0600

    New RNG testbed

--- gsl_test/rng.c
+++ gsl_test/rng.c
@@ -7,16 +7,42 @@ gsl_rng* g_rng;
 unsigned long int g_seed;
 
 int main(int argc, char** argv) {
-	if(argc < 5) { printf("Generate input data for lasso! \nusage: ./rng_test mean sd SIZE FILENAME(must be A[1234].dat and b[1234].dat) \n");
+	if(argc < 6) { printf("Generate input data for lasso! \nusage: ./rng_test mean sd SIZE FILENAME(must be A[1234].dat and b[1234].dat) Randseed [TYPE] \n");
 		printf("Example data: A1, avg=-0.00045, sd=0.08, size=20000;\n");
 		printf("              B1, avg=-0.21,    sd=0.76, size=40;\n");
+		printf("TYPE: 0 ---- Gaussian Distribution\n");
+		printf("      1 ---- Exponential Distribution (sd used as mu)\n");
 		return 0;
-	}
+	} 
 	int col=500;
 	double mean = atof(argv[1]);
 	double sd = atof(argv[2]);
 	int size = atoi(argv[3]);
 	char* filename = argv[4];
+	int randseed = atoi(argv[5]);
+
+	int type = 0;
+	if(argc >= 7) {
+		type = atoi(argv[6]);
+		switch(type) {
+		case 0:
+			printf(">> Using Gaussian Distribution.\n");
+			break;
+		case 1:
+			printf(">> Using Exponential Distribution. (mu=%g)\n",
+				sd);
+			break;
+		case 2:
+			printf(">> Using Laplacian Distribution. (a=%g\n",
+				sd);
+			break;
+		case 3:
+			printf(">> Using Cauchy Distribution. (a=%g)\n",
+				sd);
+			break;
+		}
+	}
+
 	if(filename[0]=='b') col=1;
 	int row = size / col;
 	printf("sd=%f\n", sd);
@@ -33,7 +59,20 @@ int main(int argc, char** argv) {
 	fprintf(out, "%d %d\n", row, col);
 	int i=0; for(i=0; i<size; i++) {
 		if(i%1000000==0) { printf("i=%d/%d\n", i, size); }
-		variate = gsl_ran_gaussian(g_rng, sd) + mean;
+		switch(type) {
+			case 0:
+				variate = gsl_ran_gaussian(g_rng, sd) + mean;
+				break;
+			case 1:
+				variate = gsl_ran_exponential(g_rng, sd) + mean;
+				break;
+			case 2:
+				variate = gsl_ran_laplace(g_rng, sd) + mean;
+				break;
+			case 3:
+				variate = gsl_ran_cauchy(g_rng, sd) + mean;
+				break;
+		} 
 		fprintf(out, "%f\n", variate);
 	}
 	fclose(out);
